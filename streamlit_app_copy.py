252d31303876a620b39e4b903d6bb24fd5644811
TylerKChung <85958157+TylerKChung@users.noreply.github.com>
2025-02-28 02:10:36 +0000

    Changes the app to align with directions in assignment #2

---
+++ streamlit_app_copy.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.set_page_config(
+
+    page_title='Hello world',
+    layout='centered',
+    initial_sidebar_state='auto',
+    menu_items={
+        'Get Help': 'https://streamlit.io/',
+        'Report a bug': 'https://github.com',
+        'About': 'About your application: **Hello world**'
+        }
+)
+st.sidebar.title('Hello world')
+st.sidebar.title('Tyler')
+st.title('Hello world')
